e10977525db221f1ab66e40eb2fb7a5165cd3344
Anne Koepcke <anne@koepcke.info>
2018-07-09 20:45:59 +0200

    init

---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+let mix = require('laravel-mix');
+var tailwindcss = require('tailwindcss');
+let build = require('./tasks/build.js');
+require('laravel-mix-purgecss');
+
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/');
+mix.webpackConfig({
+    plugins: [
+        build.jigsaw,
+        build.browserSync(),
+        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss']),
+    ]
+});
+
+mix.js('source/_assets/js/main.js', 'js')
+    .sass('source/_assets/sass/main.scss', 'css/main.css')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.js')],
+    })
+    .purgeCss({
+        folders: ['source'],
+    })
+    .version();
