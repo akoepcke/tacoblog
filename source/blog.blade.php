69fad87402aa02afbc897e231efb07c59f550c87
A. Koepcke <>
2019-10-10 14:53:44 +0200

    WIP update tailwind

--- source/blog.blade.php
+++ source/blog.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
     <div class="">
-        <ul class="list-reset -mb-6">
+        <ul class="-mb-6">
             @foreach ($posts as $post)
-                <li class="group bg-white rounded-lg mb-6 hover:bg-blue">
-                    <a href="{{ $post->getPath() }}" class="flex flex-col no-underline p-4 text-blue-darkest group-hover:text-white">
-                        <div class="text-lg text-blue-darker font-bold py-3">
+                <li class="group bg-white rounded-lg mb-6 hover:bg-blue-500">
+                    <a href="{{ $post->getPath() }}" class="flex flex-col no-underline p-4 text-blue-900 group-hover:text-white">
+                        <div class="text-lg text-blue-800 font-bold py-3">
                             {{ $post->title }}
                         </div>
                         <div class="">
                             {!! substr($post->getContent(), 0, 80) !!}
                         </div>
-                        <div class="text-right text-sm text-blue group-hover:text-white">
+                        <div class="text-right text-sm text-blue-500 group-hover:text-white">
                             Read more...
                         </div>
                     </a>
