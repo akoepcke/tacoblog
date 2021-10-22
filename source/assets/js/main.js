c368be0bc4ac762e6f4d35ab54ff9e044011d2ba
A. Koepcke <akoepcke@users.noreply.github.com>
2021-10-22 13:36:47 +0200

    update

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -115,8 +115,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/anne/Developer/Taco/tacoblog/source/_assets/js/main.js */"./source/_assets/js/main.js");
-module.exports = __webpack_require__(/*! /Users/anne/Developer/Taco/tacoblog/source/_assets/sass/main.scss */"./source/_assets/sass/main.scss");
+__webpack_require__(/*! /Users/anne/Sites/Taco/tacoblog/source/_assets/js/main.js */"./source/_assets/js/main.js");
+module.exports = __webpack_require__(/*! /Users/anne/Sites/Taco/tacoblog/source/_assets/sass/main.scss */"./source/_assets/sass/main.scss");
 
 
 /***/ })
